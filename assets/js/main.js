4f9552d1b193e8cec0b57252619f4979c5e9453a
Rob Quinn <Rob.M.Quinn@gmail.com>
2019-12-06 00:55:56 -0500

    added pwgenerator to portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,12 @@ $(document).ready(function(){
             $(this).css('cursor','pointer');
 })
 
+$("#pwGenerator").on("click", function() {
+    window.open('https://rquinn1017.github.io/Password_Generator/')
+    });
+
+    $('#pwGenerator').hover(function() {
+        $(this).css('cursor','pointer');
+})
+
 })
