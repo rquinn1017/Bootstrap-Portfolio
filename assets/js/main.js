83a5a811debd2a200c6f69daef9d8017f0e39a61
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-01-10 23:09:06 -0500

    deleted unused images from repo, removed unused CSS, updated portfolio to only have 3 projects and go full width. WIP sticky footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,29 @@
 
 
-$(document).ready(function(){
-    // $('.header').height($(window).height());
-    $("#quizGame").on("click", function() {
+$(document).ready(function () {
+
+    $("#quizGame").on("click", function () {
         window.open('https://rquinn1017.github.io/Quiz_Game/')
-        });
-
-        $('#quizGame').hover(function() {
-            $(this).css('cursor','pointer');
-})
-
-$("#pwGenerator").on("click", function() {
-    window.open('https://rquinn1017.github.io/Password_Generator/')
     });
 
-    $('#pwGenerator').hover(function() {
-        $(this).css('cursor','pointer');
-})
+    $('#quizGame').hover(function () {
+        $(this).css('cursor', 'pointer');
+    })
 
-$("#dayPlanner").on("click", function() {
-    window.open('https://rquinn1017.github.io/Day_Planner/')
+    $("#weatherDash").on("click", function () {
+        window.open('https://rquinn1017.github.io/WorldOfWeather/')
     });
 
-    $('#dayPlanner').hover(function() {
-        $(this).css('cursor','pointer');
-})
+    $('#weatherDash').hover(function () {
+        $(this).css('cursor', 'pointer');
+    })
 
-$("#weatherDash").on("click", function() {
-    window.open('https://rquinn1017.github.io/WorldOfWeather/')
+    $("#stateOfHappiness").on("click", function () {
+        window.open('https://baileyrva.github.io/stateofhappiness/')
     });
 
-    $('#weatherDash').hover(function() {
-        $(this).css('cursor','pointer');
-})
-
-$("#stateOfHappiness").on("click", function() {
-    window.open('https://baileyrva.github.io/stateofhappiness/')
-    });
-
-    $('#stateOfHappiness').hover(function() {
-        $(this).css('cursor','pointer');
-})
+    $('#stateOfHappiness').hover(function () {
+        $(this).css('cursor', 'pointer');
+    })
 
 })
